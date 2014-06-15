d595879b44897b4a96815cc05114736a09d6c2ec
Jeff Mataya <jeff.mataya@gmail.com>
2014-06-15 01:30:48 -0500

    Converted the factory to a provider
    Allows us to set configuration options (like if we save to local
    storage, or just log to console).

--- src/logfmt-angular.js
+++ src/logfmt-angular.js
@@ -11,34 +11,46 @@
     });
   }]);
 
-  module.factory('LogFmt', [function() {
-    return new function() {
-      this.log = function(json) {
-        var currentDate = new Date();
-        var timestamp = currentDate.getDate() + "-" +
-          (currentDate.getMonth() + 1) + "-" +
-          currentDate.getFullYear() + "-" +
-          currentDate.getHours() + ":" +
-          currentDate.getMinutes() + ":" +
-          currentDate.getSeconds();
-        var str = "timestamp=" + timestamp;
-        for(var key in json) {
-          str += " " + key + "=" + json[key];
-        }
+  module.provider('LogFmt', function() {
+    var useLocalStorage = false;
 
-        // Save to local storage.
-        var existing = localStorage["logfmt"];
-        if (existing !== undefined) {
-          existing += '\n';
-        }
-        localStorage["logfmt"] = existing + str;
-      };
-
-      this.getLogs = function() {
-        return localStorage["logfmt"];
-      }
+    this.useLocalStorage = function(value) {
+      useLocalStorage = !!value;
     }
-  }]);
+
+    this.$get = [function logFmtFactory() {
+      return new function() {
+        this.log = function(json) {
+          var currentDate = new Date();
+          var timestamp = currentDate.getDate() + "-" +
+            (currentDate.getMonth() + 1) + "-" +
+            currentDate.getFullYear() + "-" +
+            currentDate.getHours() + ":" +
+            currentDate.getMinutes() + ":" +
+            currentDate.getSeconds();
+          var str = "timestamp=" + timestamp;
+          for(var key in json) {
+            str += " " + key + "=" + json[key];
+          }
+
+          if (useLocalStorage) {
+            // Save to local storage.
+            var existing = localStorage["logfmt"];
+            if (existing !== undefined) {
+              existing += '\n';
+            }
+            localStorage["logfmt"] = existing + str;
+          } else {
+            console.log(str);
+          }
+        };
+
+        this.getLogs = function() {
+          return localStorage["logfmt"];
+        }
+      }
+    }];
+  });
 
   module.controller('LogFmtCtrl', ['LogFmt', function(LogFmt) {
     console.log("Loaded!");
